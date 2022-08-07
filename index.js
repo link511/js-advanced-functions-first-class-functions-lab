46d46d4500615f9fb52816ea4b13bbf51bd8c969
Lincoln <lincoln@taylorlivingco.com>
2022-08-07 12:56:00 +1000

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0, 2);
+}
+
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(-2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+let createFareMultiplier = function(multiValue){
+    return function (value) {
+        return value * multiValue;
+    }
+}
+
+
+let fareDoubler = createFareMultiplier(2);
+let fareTripler = createFareMultiplier(3);
+
+let selectDifferentDrivers = function(drivers, returnDriver) {
+    return returnDriver(drivers);
+    }
+
+
+   
